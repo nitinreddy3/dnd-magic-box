9d585658252f19c460d211608d35961213150d9e
nitin reddy <reddynitin3@gmail.com>
2021-03-09 19:38:32 +0530

    Added a drag and drop boxes feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import json from './stub.json'
 
 function App() {
+  const [tasks, setTasks] = useState(json.data)
+  const onDrop = (ev, cat) => {
+    let id = ev.dataTransfer.getData("id");
+
+    let taskList = tasks.filter((task) => {
+      if ((task.name === id) && (task.targetCategory === cat)) {
+        task.category = cat;
+      }
+      return task;
+    });
+
+    setTasks([...taskList])
+  };
+
+  const onDragStart = (ev, id) => {
+    console.log("dragstart:", id);
+    ev.dataTransfer.setData("id", id);
+  };
+
+  const onDragOver = (ev) => {
+    ev.preventDefault();
+  };
+
+  const getCategorizedTasks = (category) => tasks.filter(i => i.category === category).map(t => <div
+    key={t.name}
+    onDragStart={(e) => onDragStart(e, t.name)}
+    draggable
+    className="draggable"
+    style={{ backgroundColor: t.bgcolor }}
+  >
+    {t.name}
+  </div>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-drag">
+      <h2 className="header">DRAG & DROP DEMO</h2>
+      <div
+        className="wip"
+        onDragOver={(e) => onDragOver(e)}
+        onDrop={(e) => {
+          onDrop(e, "common");
+        }}
+      >
+        <span className="task-header">Common</span>
+        {getCategorizedTasks("common")}
+      </div>
+      <div
+        className="droppable"
+        onDragOver={(e) => onDragOver(e)}
+        onDrop={(e) => {
+          console.log(e)
+          onDrop(e, "categoryA")
+        }}
+      >
+        <span className="task-header">A</span>
+        {getCategorizedTasks("categoryA")}
+      </div>
+      <div
+        className="droppable"
+        onDragOver={(e) => onDragOver(e)}
+        onDrop={(e) => {
+          console.log(e)
+          onDrop(e, "categoryB")
+        }}
+      >
+        <span className="task-header">B</span>
+        {getCategorizedTasks("categoryB")}
+      </div>
     </div>
   );
 }
